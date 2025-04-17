327355e05b196ad8341a58d6854be72302e4e93d
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-17 13:31:27 -0300

    adding a find all method to the controller

--- src/main/java/study/santoliver/controllers/ProductsController.java
+++ src/main/java/study/santoliver/controllers/ProductsController.java
@@ -3,6 +3,9 @@ package study.santoliver.controllers;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import study.santoliver.model.Product;
 import study.santoliver.services.ProductService;
 
 @Controller
@@ -16,7 +19,20 @@ public class ProductsController {
 
     @RequestMapping("/products")
     public String viewProducts(Model model) {
-        var products = productService.finndAll();
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+
+    @RequestMapping(path = "/products", method = RequestMethod.POST)
+    public String addProduct(@RequestParam String name, @RequestParam double price, Model model) {
+        Product p = new Product();
+        p.setName(name);
+        p.setPrice(price);
+        productService.addProduct(p);
+
+        var products = productService.findAll();
         model.addAttribute("products", products);
 
         return "products.html";
